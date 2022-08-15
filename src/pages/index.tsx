34e500c2432e41a5b49b8a8179c46f8a8e2cf68a
Leia Harlow <leia.harlow@gmail.com>
2022-08-15 14:58:04 -0700

    installs

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import 'tailwindcss/tailwind.css';
+
+const Home: NextPage = () => {
+
+  return <div>Hello World</div>
+};
+
+
+export default Home;
